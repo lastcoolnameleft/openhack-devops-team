926dd66a4549b823a1af9d5d95cc31a24f9b7f9f
Tommy Falgout <tommy@lastcoolnameleft.com>
2020-09-10 16:58:11 -0500

    Update user.js

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -15,7 +15,7 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             res.json({
-                message: 'healthcheck8',
+                message: 'healthcheck9',
                 status: 'healthy'
             });
             callback;
